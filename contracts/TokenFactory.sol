fbb27a9fdabf3bd31f1a85ac50a748cd1b75a295
Cristiam Da Silva <cristiam@iamtech.tech>
2022-02-21 15:24:50 +0100

    removed unused library

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
